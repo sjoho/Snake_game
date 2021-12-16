8129f005f7b6c6bbf6f113be742580879468d4e2
sjoho <sjoho@sjoho.lp>
2021-12-16 23:50:29 +0300

    Барьеры

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import pygame as pg
 from pygame.locals import *
 import random
 
-pg.init()
-pg.mixer.music.load("audio/48bb90af8e1e401.mp3")
+
 
 class App:
 
     def __init__(self):
+        pg.init()
         self.clock = pg.time.Clock()
         self.WIDTH = 1200
         self.HEIGHT = 800
@@ -19,7 +19,9 @@ class App:
         self.game = True
         self.round = False
         self.font = pg.font.Font(None, 64)
+        self.walls = None
         pg.display.set_caption("SUPER SNAKE")
+        pg.mixer.music.load("audio/48bb90af8e1e401.mp3")
 
 
     def run(self):
@@ -47,17 +49,21 @@ class App:
             self.rect2 = pg.Rect(((self.WIDTH - 176) / 2, 300), (176, 100))
             self.screen.blit(self.surf2, self.rect2)
 
+            if not self.walls:
+                self.walls = [Walls() for i in range(5)]
+
             for event in pg.event.get():
                 if event.type == MOUSEBUTTONDOWN:
                     if self.rect.collidepoint(pg.mouse.get_pos()):
                         self.round = True
-                        pg.mixer.music.play(-1)
+                       # pg.mixer.music.play(-1)
                     if self.rect2.collidepoint(pg.mouse.get_pos()):
                         pg.quit()
+                elif event.type == QUIT:
+                    pg.quit()
 
             pg.display.update()
             while self.round:
-
 
                 if not self.fruit:
                     self.fruit = Fruit()
@@ -90,10 +96,20 @@ class App:
                     self.surf.fill((50, 50, 50))
                     self.snake.snake.append((self.rect, self.surf))
 
+                for i in self.walls:
+                    if i.rect.colliderect(self.snake.rect):
+                        self.round = False
+                        self.snake.__init__()
+                        self.walls = []
+                    elif self.fruit and i.rect.colliderect(self.fruit.rect):
+                        self.fruit = None
 
                 self.screen.blit(self.background, (0, 0))
+
                 if self.fruit:
                     self.fruit.draw()
+                for i in self.walls:
+                    i.draw()
                 self.snake.draw()
                 pg.display.update()
                 self.clock.tick(30)
@@ -122,6 +138,7 @@ class Snake:
         self.surf.fill((0, 0, 0))
         self.destination = "RIGHT"
         self.snake = [(self.rect, self.surf)]
+        self.get_touched = False
 
 
     def draw(self):
@@ -132,22 +149,28 @@ class Snake:
             if self.rect[0] <= app.WIDTH + 25:
                 self.rect[0] += self.speed
             else:
-                self.rect[0] = -25
+                self.get_touched = True
         elif self.destination == "DOWN":
             if self.rect[1] <= app.HEIGHT + 25:
                 self.rect[1] += self.speed
             else:
-                self.rect[1] = -25
+                self.get_touched = True
         elif self.destination == "UP":
             if self.rect[1] >= 0:
                 self.rect[1] -= self.speed
             else:
-                self.rect[1] = app.HEIGHT
+                self.get_touched = True
         elif self.destination == "LEFT":
             if self.rect[0] >= 0:
                 self.rect[0] -= self.speed
             else:
-                self.rect[0] = app.WIDTH
+                self.get_touched = True
+
+        if self.get_touched:
+            app.round = False
+            pg.mixer.music.stop()
+            self.__init__()
+            app.walls = []
 
         if len(self.snake) > 1:
             for i in range(0, len(self.snake) - 1):
@@ -158,6 +181,27 @@ class Snake:
 
         for j in self.snake:
             app.screen.blit(j[1], j[0])
+
+
+class Walls:
+
+    def __init__(self):
+        self.size = random.randint(0, 1)
+        if self.size == 0:
+            self.coord_x = random.randint(0, app.WIDTH - 100)
+            self.coord_y = random.randint(0, app.HEIGHT - 80)
+            self.rect = pg.Rect((self.coord_x, self.coord_y), (app.HEIGHT / 40, app.WIDTH / 6))
+            self.surf = pg.Surface((app.HEIGHT / 40, app.WIDTH / 6))
+            self.surf.fill((0, 120, 0))
+        elif self.size == 1:
+            self.coord_x = random.randint(0, app.WIDTH - 100)
+            self.coord_y = random.randint(0, app.HEIGHT - 80)
+            self.rect = pg.Rect((self.coord_x, self.coord_y), (app.HEIGHT / 4, app.WIDTH / 60))
+            self.surf = pg.Surface((app.HEIGHT / 4, app.WIDTH / 60))
+            self.surf.fill((0, 120, 0))
+
+    def draw(self):
+        app.screen.blit(self.surf, self.rect)
 
 
 app = App()
